9536267978a23c8bfd537a29009fbe432de859cf
Alexandr Notchenko <avnotchenko@gmail.com>
2019-02-25 18:54:22 +0300

    Update of passport data

--- rankings/passport.py
+++ rankings/passport.py
@@ -10,7 +10,7 @@ class PassportIndex(IndividualRanking):
     higher_is_better = True
 
     def __init__(self):
-        self.df = pd.read_csv('./Raw_Data/passport-index-country-names.csv',header=0, index_col=0)
+        self.df = pd.read_csv('./Raw_Data/passport-index-ISO-alpha-3.csv',header=0, index_col=0)
 
     def get_ranking(self, hold_passports=('Russian Federation',), wights_to_countries: Optional[Dict]=None):
         new_df = reduce(or_, [(self.df.loc[_country] > 0) for _country in hold_passports], (self.df > 0))
